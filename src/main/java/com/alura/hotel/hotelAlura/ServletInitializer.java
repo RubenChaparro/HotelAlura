87cabeae50bd018bc94234622756eccd2eeb3532
Ruben <marte23122010@gmail.com>
2023-06-13 18:14:36 -0500

    Creando Hotel Alura

---
+++ src/main/java/com/alura/hotel/hotelAlura/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.alura.hotel.hotelAlura;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HotelAluraApplication.class);
+	}
+
+}
